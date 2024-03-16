ca3cdbb85b6e35c3f49813dce8351d548e5d0c0b
gsanthprakash <153313380+gspvsr@users.noreply.github.com>
2024-03-16 16:19:05 -0500

    messages

--- catalogue.sh
+++ catalogue.sh
@@ -4,32 +4,30 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-MONGODB_HOST=gspaws.online
-
-LOGSDIR=/home/centos/gsp
-SHELL_SCRIPT=$0
-DATE=$(date +%F)
-LOGFILE=$LOGSDIR/$SHELL_SCRIPT-$DATE.log
+MONGODB_HOST=mongodb.gspaws.online
 
 USERID=$(id -u)
-
-mkdir -p "$LOGSDIR"
-
-if [[ $USERID -ne 0 ]]
-then 
-    echo -e "$R ERROR, please proceed with root access $N"
-else    
-    echo -e "$G You are root user $N"
-fi
+LOGSDIR=/home/centos/Shell_Script
+SHELL_SCRIPT=$0
+DATE=$(date +%F)
+LOGFILE="$LOGSDIR/$SHELL_SCRIPT-$DATE.log"
 
 VALIDATE(){
     if [[ $1 -ne 0 ]]
     then 
-        echo -e "$2 $R ... FAILURE $N"
+        echo -e "$2 $R ... FAILED $N"
     else
         echo -e "$2 $G .... SUCCESS $N"
     fi
 }
+
+if [[ $USERID -ne 0 ]]
+then 
+    echo -e "$R ERROR, please proceed with root access $N"
+    exit 1
+else    
+    echo -e "$G You are root user $N"
+fi
 
 dnf module disable nodejs -y &>>$LOGFILE
 VALIDATE $? "disabling the nodejs latest"
@@ -40,28 +38,28 @@ VALIDATE $? "enabling nodejs:18"
 dnf install nodejs -y &>>$LOGFILE
 VALIDATE $? "Installing Nodejs"
 
+id robosho # if roboshop user does not exists, then it is failure.
 if [[ $? -ne 0 ]]
 then 
     useradd roboshop
-    VALIDATE $? "useradding to roboshop"
+    VALIDATE $? "roboshop user creation"
 else
     echo -e "$G user already exisits, skipping $N"
 fi
 
-mkdir -vp /app &>>$LOGFILE
+mkdir -p /app &>>$LOGFILE
 VALIDATE $? "app directory adding"
 
 curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
 VALIDATE $? "downloading the catalouge"
 
-cd /app &>>$LOGFILE
+cd /app
 VALIDATE $? "entering to app directory"
 
 unzip -o /tmp/catalogue.zip &>>$LOGFILE
 VALIDATE $? "unzipping the catalogue"
 
 npm install &>>$LOGFILE
-VALIDATE $? "NPM installing"
 
 cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
 VALIDATE $? "copying the catalogue.service"
