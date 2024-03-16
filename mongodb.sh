6f857aaa17a724de45e0bd6a464517da3c1c3048
gsanthprakash <153313380+gspvsr@users.noreply.github.com>
2024-03-15 22:31:59 -0500

    messages

--- mongodb.sh
+++ mongodb.sh
@@ -29,5 +29,20 @@ else
     echo -e $G You are root user $N
 fi
 
-yum install mongodb -y &&>>$LOGFILE
-VALIDATE $?
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
+VALIDATE $? "Copies Mongo.repo to mongo.repos.d folder"
+
+yum install mongodb-org -y &>>$LOGFILE
+VALIDATE $? "Installed Mongodb-org" 
+
+systemctl enable mongod &>>$LOGFILE
+VALIDATE $? "Enabled mongdb"
+
+systemctl start mongod &>>$LOGFILE
+VALIDATE $? "Started the mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOGFILE
+VALIDATE $? "edited the sed editor"
+
+systemctl restart mongod &>>$LOGFILE
+VALIDATE $? "Restarted the Mongodb"
