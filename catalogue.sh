cf43f21841e12dca1f5ce5516b446923ecbf5237
gsanthprakash <153313380+gspvsr@users.noreply.github.com>
2024-03-15 23:25:01 -0500

    messages

--- catalogue.sh
+++ catalogue.sh
@@ -23,56 +23,56 @@ fi
 VALIDATE(){
     if [[ $1 -ne 0 ]]
     then 
-        echo "$2 $R ... FAILURE $N"
+        echo -e "$2 $R ... FAILURE $N"
     else
-        echo "$2 $G .... SUCCESS $N"
+        echo -e "$2 $G .... SUCCESS $N"
     fi
 }
 
-dnf module disable nodejs -y $>>$LOGFILE
+dnf module disable nodejs -y &>>$LOGFILE
 VALIDATE $? "disabling the nodejs latest"
 
-dnf module enable nodejs:18 -y $>>$LOGFILE
+dnf module enable nodejs:18 -y &>>$LOGFILE
 VALIDATE $? "enabling nodejs:18"
 
-dnf install nodejs -y $>>$LOGFILE
+dnf install nodejs -y &>>$LOGFILE
 VALIDATE $? "Installing Nodejs"
 
-useradd -M roboshop $>>$LOGFILE
+useradd -M roboshop &>>$LOGFILE
 VALIDATE $? "useradding to roboshop"
 
-mkdir -vp /app >> $LOGFILE
+mkdir -vp /app >> &>>$LOGFILE
 VALIDATE $? "app directory adding"
 
-curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
 VALIDATE $? "downloading the catalouge"
 
-cd /app $>>$LOGFILE
+cd /app &>>$LOGFILE
 VALIDATE $? "entering to app directory"
 
-unzip -o /tmp/catalogue.zip >> $LOGFILE
+unzip -o /tmp/catalogue.zip >> &>>$LOGFILE
 VALIDATE $? "unzipping the catalogue"
 
-npm install $>>$LOGFILE
+npm install &>>$LOGFILE
 VALIDATE $? "NPM installing"
 
-cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service $>>$LOGFILE
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
 VALIDATE $? "copying the catalogue.service"
 
-systemctl daemon-reload $>>$LOGFILE
+systemctl daemon-reload &>>$LOGFILE
 VALIDATE $? "deamon reloading"
 
-systemctl enable catalogue $>>$LOGFILE
+systemctl enable catalogue &>>$LOGFILE
 VALIDATE $? "enabling catalogue"
 
-systemctl start catalogue $>>$LOGFILE
+systemctl start catalogue &>>$LOGFILE
 VALIDATE $? "Starting the catalogue"
 
-cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo $>>$LOGFILE
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
 VALIDATE $? "copying the mongo.repo"
 
-dnf install mongodb-org-shell -y $>>$LOGFILE
+dnf install mongodb-org-shell -y &>>$LOGFILE
 VALIDATE $? "Installing the Mongodb-Org-Shell"
 
-mongo --host $MONGODB_HOST </app/schema/catalogue.js $>>$LOGFILE
+mongo --host $MONGODB_HOST </app/schema/catalogue.js &>>$LOGFILE
 VALIDATE $? "connecting the mongdb Server"
