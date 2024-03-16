66be2b09e483214991a91ee8e1beac4c80452da1
gsanthprakash <153313380+gspvsr@users.noreply.github.com>
2024-03-15 21:50:53 -0500

    messages

---
+++ mongodb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+USERID=$(id -u)
+DATE=$(date +%F)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[33m"
+
+LOGSDIR=/home/centos/shell-script
+SCRIPT_NAME=$0
+LOGSFILE=$LOGSDIDR/$SCRIPT_NAME-$DATE.log
+
+VALIDATE(){
+    if [$USERID -ne 0];
+    then
+        [$R ERROR: please proceed with Root user $N]
+    else
+        [$G You are root user $N]
+    fi
+}
