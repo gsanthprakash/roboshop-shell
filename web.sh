ed44f6aab36fe425f4c1beb4938494208038b673
gsanthprakash <153313380+gspvsr@users.noreply.github.com>
2024-03-16 17:17:57 -0500

    messages

---
+++ web.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+R="\e[31m"
+Y="\e[32m"
+G="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+LOGSDIR=/tmp/
+SCRIPT_NAME=$0
+DATE=$(date +%F)
+LOGFILE=$LOGSDIDR/$SCRIPT_NAME-$DATE.log
+
+VALIDATION(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e " $R $2 ...FAILED $N "
+        exit 1
+    else
+        echo -e " $G $2 ...SUCCESS $N "
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then 
+    echo -e " $R installation failure $N "
+    exit 1
+echo
+    echo -e " $G installation success $N "
+fi
+
+dnf install nginx -y &>> $LOGFILE
+VALIDATE $? "Installing the nginx"
+
+systemctl enable nginx &>> $LOGFILE
+VALIDATE $? "enabling nginx"
+
+systemctl start nginx &>> $LOGFILE
+VALIDATE $? "starting the nginx"
+
+rm -rf /usr/share/nginx/html/* &>> $LOGFILE
+VALIDATE $? "removing the default nginx"
+
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>> $LOGFILE
+VALIDATE $? "download the roboshop builds"
+
+cd /usr/share/nginx/html &>> $LOGFILE
+VALIDATE $? "moving to nginx directory"
+
+unzip -o /tmp/web.zip &>> $LOGFILE
+VALIDATE $? "unzipping the web.zip"
+
+cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>> $LOGFILE
+VALIDATE $? "copying the roboshop.conf"
+
+systemctl restart nginx &>> $LOGFILE
+VALIDATE $? "restaring the nginx"
+
+
+
+
