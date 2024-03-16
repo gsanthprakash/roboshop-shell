7856ace5151bf0aed49e20016b3bb17e52f926fc
gsanthprakash <153313380+gspvsr@users.noreply.github.com>
2024-03-15 21:56:03 -0500

    messages

--- mongodb.sh
+++ mongodb.sh
@@ -5,20 +5,20 @@ DATE=$(date +%F)
 
 R="\e[31m"
 G="\e[32m"
-N="\e[33m"
+N="\e[0m"
 
 LOGSDIR=/home/centos/shell-script
 SCRIPT_NAME=$0
 LOGSFILE=$LOGSDIDR/$SCRIPT_NAME-$DATE.log
 
-VALIDATE(){
-    if [$USERID -ne 0];
-    then
-        [$R ERROR: please proceed with Root user $N]
-    else
-        [$G You are root user $N]
-    fi
-}
+if [$USERID -ne 0]
+then
+    echo $R ERROR: please proceed with Root user $N
+    exit 1
+else
+    echo $G You are root user $N
+fi
+
 
 yum install mongodb -y &&>>$LOGFILE
 VALIDATE $?
